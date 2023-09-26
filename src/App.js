7688b6e335e20a87d1415f13bc7e19b4dcd7623b
Iheb ZARGOUNI <iheb.zargouni.pr@gmail.com>
2023-09-26 15:47:30 +0100

    Next Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import React from 'react';
 import './App.css';
+import Profile from './Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Russell Crowe',
+        bio: 'A passionate individual with various interests.',
+        imgSrc: require('./profile.jpg'),
+        profession: 'Software Engineer',
+      },
+      show: true,
+      timeMark: null
+    };
+  }
+
+
+  showProfile = () => {
+    (this.state.show) ? this.setState({ show: false }) : this.setState({ show: true });
+  }
+
+  componentDidMount() {
+    this.setState({
+      timeMark: new Date(),
+    });
+    this.interval = setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="firstDiv">
+          <div className="underDiv1">
+            {this.state.show ? <Profile {...this.state.person} timer={this.state.timer} /> : null}
+          </div>
+          <div className="underDiv2">
+            <Button variant="secondary" size="lg" className="Toggler" onClick={this.showProfile}>Button</Button>
+          </div>
+          <div className="underDiv3">
+            <td className="td1"><p>Time interval since mount:</p></td>
+            <td className="td2">{Math.floor((new Date() - this.state.timeMark) / 1000)}</td>
+            <td className="td3"><p>seconds</p></td>
+          </div>
+        </div>
+      </div >
+    );
+  }
 }
 
 export default App;
